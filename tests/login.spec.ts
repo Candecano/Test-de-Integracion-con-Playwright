3afc36ddc17c74b06d11d5342c77ca4494471164
Candecano <candecano11@gmail.com>
2025-03-25 23:19:28 -0300

    test validacion ingreso exitoso

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,35 +1,37 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 dotenv.config(); // Carga variables del .env
 
 const BASE_URL = process.env.BASE_URL ?? "";
-const DNIVALIDO1 = process.env.DNIVALIDO1 ?? "";
-const PASS_VALIDO = process.env.PASS_VALIDO ?? "";
-const DNI_INVALIDO = process.env.DNI_INVALIDO ?? "";
+const DNIVALIDO = process.env.DNIVALIDO ?? "";
+const PVALIDO = process.env.PVALIDO ?? "";
+const DNIINVALIDO1 = process.env.DNIINVALIDO1 ?? "";
+const DNIINVALIDO2= process.env.DNIINVALIDO2 ?? "";
 const PASS_INVALIDO = process.env.PASS_INVALIDO ?? "";
-const DNIVALIDO2= process.env.DNIVALIDO2 ?? "";
-const PASS_VALIDO2 = process.env.PASS_VALIDO2 ?? "";
+
 
 test.describe('Pruebas de Login', () => {
     
-    test('Caso exitoso: ingreso con credenciales válidas 1', async ({ page }) => {
-        await page.goto(`${BASE_URL}`);
-        await page.pause()
-        await page.getByRole('textbox', { name: 'Usuario' }).fill(DNIVALIDO1);
-        await page.getByRole('textbox', { name: 'Clave inicial' }).fill(PASS_VALIDO);
-        await page.getByRole('button', { name: 'Submit' }).click();
-        
+    test('Caso exitoso: ingreso con credenciales válidas ', async ({ page }) => {
+  
+const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+ 
+await page.goto(`${BASE_URL}`);
+await page.pause()
+await page.locator('[placeholder="Usuario"]').fill(DNIVALIDO); 
+await page.locator('[placeholder="Contraseña"]').fill(PVALIDO);
+await page.getByRole('button', { name: 'Submit' }).click();
+await context.close();
+await browser.close();
+  
 
     });
 
-    test('Caso exitoso: ingreso con credenciales válidas 2', async ({ page }) => {
-        await page.goto(`${BASE_URL}`);
-        await page.pause()
-        await page.getByRole('textbox', { name: 'Usuario' }).fill(DNIVALIDO2);
-        await page.getByRole('textbox', { name: 'Clave inicial' }).fill(PASS_VALIDO2);
-        await page.getByRole('button', { name: 'Submit' }).click();
-        
-
-    });
+   
 });
+
+
